038dc998f03e903ddc77e3e50a33f8eafc064e30
banisle <banisle@gmail.com>
2020-11-07 02:38:03 +0900

    메인 gnb

---
+++ static/js/common.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	// gnb
+	ANUI.module.gnbAllUi();
+
+	var $nav = $('.gnb'),
+    $a = $nav.find('> ul > li > a'),
+    $sBar = $nav.find('.floatBar');
+
+    $a.on('mouseover', function(){
+        var aW = $(this).width(),
+            aL = $(this).offset().left;
+
+        $sBar.css({
+            'width': aW,
+            'left' : aL - 160
+		});
+	});
+
+
+});
