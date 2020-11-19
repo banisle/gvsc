11cc114e917299eb27a2ad390bfddc6904416313
banisle <banisle@gmail.com>
2020-11-19 01:45:57 +0000

    update: 모바일 메뉴 ing

--- static/js/common.js
+++ static/js/common.js
@@ -1,21 +1,31 @@
 $(document).ready(function(){
 	// gnb
-	ANUI.module.gnbAllUi();
+    ANUI.module.gnbAllUi();
 
-	var $nav = $('.gnb'),
-    $a = $nav.find('> ul > li > a'),
-    $sBar = $nav.find('.floatBar');
+    var $moBtn = $('.mo-menu'),
+        $headerGnb = $('header .gnb');
 
-    $a.on('mouseover', function(){
-		var _t = $(this),
-			_tP = $(this).parent(),
-			aW = _t.width(),
-            aL = _tP.position().left;
+    $moBtn.on('click', function(){
+        $headerGnb.toggleClass('on');
+    });
 
-        $sBar.css({
-            'width': aW,
-            'left' : aL + 100
-		});
-	});
+    if (!isMobile) {
+
+        var $nav = $('.gnb'),
+        $a = $nav.find('> ul > li > a'),
+        $sBar = $nav.find('.floatBar');
+
+        $a.on('mouseover', function(){
+            var _t = $(this),
+                _tP = $(this).parent(),
+                aW = _t.width(),
+                aL = _tP.position().left;
+
+            $sBar.css({
+                'width': aW,
+                'left' : aL + 100
+            });
+        });
+    }
 
 });
